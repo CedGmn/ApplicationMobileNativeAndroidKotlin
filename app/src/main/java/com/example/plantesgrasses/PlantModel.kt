f8253f37ce7aefe2cbdafbed9b1c9e286ddebd02
CedGmn <cedricguilleminpro@gmail.com>
2021-03-01 12:36:08 +0100

    ajout d'images différentes via Uri (lien iternet)

---
+++ app/src/main/java/com/example/plantesgrasses/PlantModel.kt
@@ -0,0 +1,9 @@
+package com.example.plantesgrasses
+
+class PlantModel(
+        val name : String = "Catus 1",
+        val description : String = "Description",
+        val imageUrl: String = "https://cdn.pixabay.com/photo/2017/08/08/00/57/plants-2609843_960_720.jpg",
+        val liked : Boolean = false
+
+)
